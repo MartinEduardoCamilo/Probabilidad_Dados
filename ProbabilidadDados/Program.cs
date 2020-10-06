ccbcec107b99167cefb8cab5e02266389350123d
Martin Eduardo Camilo De La Cruz <martin_camilo@ucne.edu.do>
2020-10-05 22:32:03 -0400

    Add project files.

---
+++ ProbabilidadDados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ProbabilidadDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados();
+        }
+
+        private static void Dados()
+        {
+            int numero = 0;
+            double probabilidad = 0.0;
+
+            int[] lista = { 1, 2, 3, 4, 5, 6 };
+
+            //Console.WriteLine("Inserete un numero: ");
+            //numero = Convert.ToInt32(Console.Read());
+
+            //Console.WriteLine("Inserete la probabilidad: ");
+            //probabilidad = Convert.ToDouble(Console.Read());
+
+            //Console.WriteLine("////");
+
+            for(int i =0; i > 6 ; i++)
+            {
+                Console.WriteLine("jjj");
+            }
+        }
+    }
+}
